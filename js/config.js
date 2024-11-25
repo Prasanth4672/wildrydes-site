3b0edeef8d69d0ba7406a9a50675210f83cb58d9
Prasanth4672 <133503309+Prasanth4672@users.noreply.github.com>
2024-11-25 12:05:20 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_qFqA9UDFq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4hmohe2bqff7gtn5vt2audqohr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5m4b9l4ng431fpbp8n5ig2l5k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
