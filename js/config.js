180001a619539aeb206567a107c3d1bd16d2f8d8
Prasanth4672 <133503309+Prasanth4672@users.noreply.github.com>
2024-11-25 11:58:41 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_qFqA9UDFq', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4hmohe2bqff7gtn5vt2audqohr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
